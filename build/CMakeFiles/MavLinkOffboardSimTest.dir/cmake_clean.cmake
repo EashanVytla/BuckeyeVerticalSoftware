ce9536d1e7bae86fb71574d837b9b6c496797c46
anthonydfox <anthonydfox04@gmail.com>
2024-02-15 20:36:36 -0500

    Good prelimenary work Offboard

---
+++ build/CMakeFiles/MavLinkOffboardSimTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MavLinkOffboardSimTest.dir/src/MavLinkOffboardSimTest.cpp.o"
+  "CMakeFiles/MavLinkOffboardSimTest.dir/src/MavLinkOffboardSimTest.cpp.o.d"
+  "MavLinkOffboardSimTest"
+  "MavLinkOffboardSimTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MavLinkOffboardSimTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/PWMPeriodFinder.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for PWMPeriodFinder.
+# This may be replaced when dependencies are built.
