88975980248a10452ecb28a7d2bdc97610fe0e39
Sean Broderick <133047023+seanbrod@users.noreply.github.com>
2024-03-05 02:06:39 -0800

    Update detect.cpp

--- src/detect.cpp
+++ src/detect.cpp
@@ -1,7 +1,6 @@
-#include "detect.hpp"
-#include <cassert>
-#include <chrono>
-#include <opencv2/opencv.hpp>
+#include "detect.h"
+#include "chrono"
+#include "opencv2/opencv.hpp"
 #include "yolov8.hpp"
 
 const std::vector<std::string> CLASS_NAMES = {
@@ -16,10 +15,11 @@ const std::vector<std::string> CLASS_NAMES = {
     "chair",          "couch",      "potted plant",  "bed",           "dining table", "toilet",        "tv",
     "laptop",         "mouse",      "remote",        "keyboard",      "cell phone",   "microwave",     "oven",
     "toaster",        "sink",       "refrigerator",  "book",          "clock",        "vase",          "scissors",
-    "teddy bear",     "hair drier", "toothbrush"};
+    "teddy bear",     "hair drier", "toothbrush"
+};
 
 const std::vector<std::vector<unsigned int>> COLORS = {
-    {0, 114, 189},   {217, 83, 25},   {237, 177, 32},  {126, 47, 142},  {119, 172, 48},  {77, 190, 238},
+   {0, 114, 189},   {217, 83, 25},   {237, 177, 32},  {126, 47, 142},  {119, 172, 48},  {77, 190, 238},
     {162, 20, 47},   {76, 76, 76},    {153, 153, 153}, {255, 0, 0},     {255, 128, 0},   {191, 191, 0},
     {0, 255, 0},     {0, 0, 255},     {170, 0, 255},   {85, 85, 0},     {85, 170, 0},    {85, 255, 0},
     {170, 85, 0},    {170, 170, 0},   {170, 255, 0},   {255, 85, 0},    {255, 170, 0},   {255, 255, 0},
@@ -32,15 +32,15 @@ const std::vector<std::vector<unsigned int>> COLORS = {
     {0, 128, 0},     {0, 170, 0},     {0, 212, 0},     {0, 255, 0},     {0, 0, 43},      {0, 0, 85},
     {0, 0, 128},     {0, 0, 170},     {0, 0, 212},     {0, 0, 255},     {0, 0, 0},       {36, 36, 36},
     {73, 73, 73},    {109, 109, 109}, {146, 146, 146}, {182, 182, 182}, {219, 219, 219}, {0, 114, 189},
-    {80, 183, 189},  {128, 128, 0}};
+    {80, 183, 189},  {128, 128, 0}
+};
 
-int detect::inference(int argc, char** argv)
-{
+int detect::inference(int argc, char** argv) {
     const std::string engine_file_path{argv[1]};
     const std::string path{argv[2]};
 
     std::vector<std::string> imagePathList;
-    bool isVideo{false};
+    bool                     isVideo{false};
 
     assert(argc == 3);
 
@@ -61,8 +61,8 @@ int detect::inference(int argc, char** argv)
         cv::glob(path + "/*.jpg", imagePathList);
     }
 
-    cv::Mat res, image;
-    cv::Size size = cv::Size{640, 640};
+    cv::Mat             res, image;
+    cv::Size            size = cv::Size{640, 640};
     std::vector<Object> objs;
 
     cv::namedWindow("result", cv::WINDOW_AUTOSIZE);
@@ -75,10 +75,12 @@ int detect::inference(int argc, char** argv)
             return -1;
         }
 
+        // Get the video frame size and FPS
         cv::Size frameSize = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
         double fps = cap.get(cv::CAP_PROP_FPS);
 
-        cv::VideoWriter writer("output.mp4", cv::VideoWriter::fourcc('H', '2', '6', '5'), fps, frameSize, true);
+        // Initialize the VideoWriter object (change the filename and codec as needed)
+        cv::VideoWriter writer("output.mp4", cv::VideoWriter::fourcc('H', '2', '6', '5'), fps, frameSize, true);//can change to MP4 format here instead of H.265
 
         while (cap.read(image)) {
             objs.clear();
@@ -91,6 +93,7 @@ int detect::inference(int argc, char** argv)
             auto tc = (double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.;
             printf("cost %2.4lf ms\n", tc);
 
+            // Write the annotated frame to the video
             writer.write(res);
 
             cv::imshow("result", res);
@@ -99,6 +102,7 @@ int detect::inference(int argc, char** argv)
             }
         }
 
+        // Release the video writer
         writer.release();
     } else {
         for (auto& path : imagePathList) {
